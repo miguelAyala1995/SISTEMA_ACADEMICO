12ef035cc25e2d3f67faf3afbd1049da5c88d4f9
miguelAyala1995 <113142227+miguelAyala1995@users.noreply.github.com>
2022-11-18 12:33:12 -0500

    Delete include directory

--- include/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-// conexion utiliza servidor-usuario-password-base de datos
-$conexion = mysqli_connect("localhost", "miguel", "HaHPgRudAV?f", "ceicom_miguel");
-if ($conexion) {
-
-} else {
-   echo "conexion fallida";
-}
-?>
